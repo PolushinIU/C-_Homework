0088cea8e98e0ac15d0eaa52d34b08db21f1c111
Иван <polushinv@gmail.com>
2022-06-08 18:40:54 +0300

    выполнил четвертую задачу

---
+++ Task_4/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая на вход
+// принимает число (N), а на выходе показывает все чётные
+// числа от 1 до N.
+Console.Write("Введите первое число => ");
+string paramString = Console.ReadLine();
+int paramInt = int.Parse(paramString);
+
+while (paramInt > 0)
+if (paramInt % 2 == 1)
+{
+    paramInt = paramInt - 1;
+}
+
+else
+{
+    Console.Write($"{paramInt}  ");
+    paramInt = paramInt - 1;
+}
+
+
+
+
